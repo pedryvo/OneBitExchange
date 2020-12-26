accca77b9d33987c50afe72725de21ac0bc91280
Pedro Ivo <pedryvo@gmail.com>
2020-12-26 02:08:32 -0300

    create ajax and fix forms

--- app/controllers/exchanges_controller.rb
+++ app/controllers/exchanges_controller.rb
@@ -3,7 +3,6 @@ class ExchangesController < ApplicationController
   end
  
   def convert
-    value = ExchangeService.new(params[:source_currency], params[:target_currency], params[:amount]).perform
-    render json: {"value": value}
+    @value = ExchangeService.new(params[:source_currency], params[:target_currency], params[:amount]).perform
   end
 end
