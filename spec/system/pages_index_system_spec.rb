29d2c1859fafeac44002abeadf4483207a8eee19
Pedro Ivo <pedryvo@gmail.com>
2020-12-26 02:11:48 -0300

    fix system spec

--- spec/system/pages_index_system_spec.rb
+++ spec/system/pages_index_system_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Exchange Currency Process", :type => :system, js: true do
   it "exchange value" do
     visit '/'
-    within("#exchange_form") do
+    within("#main_form") do
       select('EUR', from: 'source_currency')
       select('USD', from: 'target_currency')
       fill_in 'amount', with: rand(1..9999)
